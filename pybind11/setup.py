dbfafa7fc1ca807a0dc5caaa9732c3c2ebda98a3
Kee Myoung Nam <kmnam@g.harvard.edu>
2021-12-08 23:03:21 +0900

    First commit

---
+++ pybind11/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+ext_modules = [
+    Pybind11Extension(
+        'pygraph',
+        ['pygraph.cpp'],
+        include_dirs=[
+            '/usr/local/Cellar/boost/1.76.0/include',
+            '/usr/local/Cellar/eigen/3.4.0_1/include/eigen3'
+        ],
+        extra_compile_args=[
+            '--std=c++11'
+        ],
+        extra_link_args=[
+            '-lmpfr'
+        ]
+    )
+]
+
+setup(
+    name='pygraph',
+    version='0.9b',
+    ext_modules=ext_modules
+)
