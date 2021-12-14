2f117a3fb696e1df07f681158c55142d925fc92f
Kee Myoung Nam <kmnam@g.harvard.edu>
2021-12-14 23:30:05 +0900

    Added graphviz requirements

--- setup.py
+++ setup.py
@@ -7,13 +7,16 @@ ext_modules = [
         ['src/pygraph.cpp'],
         include_dirs=[
             '/usr/local/Cellar/boost/1.76.0/include',
-            '/usr/local/Cellar/eigen/3.4.0_1/include/eigen3'
+            '/usr/local/Cellar/eigen/3.4.0_1/include/eigen3',
+            '/usr/local/Cellar/graphviz/2.40.1/include'
         ],
         extra_compile_args=[
             '--std=c++11'
         ],
         extra_link_args=[
-            '-lmpfr'
+            '-lmpfr',
+            '-lgvc',
+            '-lcgraph'
         ]
     )
 ]
