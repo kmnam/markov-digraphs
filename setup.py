ba6abea37593f5b74ce818607955612accf3613e
Kee Myoung Nam <kmnam@g.harvard.edu>
2021-12-15 19:59:24 +0900

    Slight changes

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
+BOOST_INCLUDE = '/usr/local/Cellar/boost/1.76.0/include'
+EIGEN_INCLUDE = '/usr/local/Cellar/eigen/3.4.0_1/include/eigen3'
+GRAPHVIZ_INCLUDE = '/usr/local/Cellar/graphviz/2.40.1/include'
+
+# Infer GRAPHVIZ_LIB from GRAPHVIZ_INCLUDE (which should end with /include)
+GRAPHVIZ_LIB = GRAPHVIZ_INCLUDE[:-8] + '/lib'
+
 ext_modules = [
     Pybind11Extension(
         'pygraph',
         ['src/pygraph.cpp'],
-        include_dirs=[
-            '/usr/local/Cellar/boost/1.76.0/include',
-            '/usr/local/Cellar/eigen/3.4.0_1/include/eigen3',
-            '/usr/local/Cellar/graphviz/2.40.1/include'
-        ],
+        include_dirs=[BOOST_INCLUDE, EIGEN_INCLUDE, GRAPHVIZ_INCLUDE],
+        library_dirs=[GRAPHVIZ_LIB],
         extra_compile_args=[
             '--std=c++11'
         ],
